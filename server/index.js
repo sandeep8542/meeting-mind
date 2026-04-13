29be87c2019e66f40ef4b3601400f6b67bd73de9
Sandeep Kuamar <shiva190986@email.com>
2026-04-14 01:17:26 +0530

    remove frontend static serving

--- server/index.js
+++ server/index.js
@@ -26,13 +26,6 @@ app.use("/api/ai", require("./routes/ai"));
 app.use("/api/actions", require("./routes/actionRoutes"));
 const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
 
 // ✅ Test route
 app.get("/", (req, res) => {
